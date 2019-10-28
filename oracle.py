587c46805dd8c60c6a6bab04dcd1ca64226632e9
Gulen <guleninsay@hotmail.co.uk>
2019-10-28 15:24:08 +0000

    Change variable name

--- oracle.py
+++ oracle.py
@@ -1,8 +1,8 @@
 from random import randint
 
 def ask():
-	future = ['go for it', 'don\'t you dare', 'no', 'yes', 'absolutely','absolutely not','why not?','please do not','i do not know','try again','are you crazy?','would not be my first choice', 'would be the end of you','great idea','you should','not sure','maybe, maybe not', 'I don\'t wanna talk about it']
-	print(future[randint(0, len(future) - 1)])
+	ans = ['go for it', 'don\'t you dare', 'no', 'yes', 'absolutely','absolutely not','why not?','please do not','i do not know','try again','are you crazy?','would not be my first choice', 'would be the end of you','great idea','you should','not sure','maybe, maybe not', 'I don\'t wanna talk about it']
+	print(ans[randint(0, len(ans) - 1)])
 	return 
 
 	
